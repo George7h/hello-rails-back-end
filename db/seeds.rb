6c5af162f9a427666558757c6dad7628d60344f4
George <georgie.george7@gmail.com>
2024-02-23 09:30:05 +0400

    Add greetings table and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+greeting1 = Greeting.create!(message: "Hello, Welcome")
+greeting2 = Greeting.create!(message: "Good Morning")
+greeting3 = Greeting.create!(message: "Good Afternoon")
+greeting4 = Greeting.create!(message: "Good Evening")
+greeting5 = Greeting.create!(message: "Goodbye")
